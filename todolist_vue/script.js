a31e995011cbda5727fdfaaa9d2dcb04f2fc9b36
vignesh0208 <v19ne5h1994@gmail.com>
2019-10-31 09:45:58 +0530

    modified in todo list file

--- todolist_vue/script.js
+++ todolist_vue/script.js
@@ -3,14 +3,26 @@ var app = new Vue({
     data:{
         userInput: "",
         items:[],
+        status: true,
+        index: ''
     },
     methods:{
         addTask: function(x) {
             this.items.push(x)
             this.userInput = ''
         },
-        removeTask: function(){
-            this.items.pop()
+        removeTask: function(index) {
+            this.items.splice(index, 1)
+        },
+        editTask: function(index) {
+            this.status= false
+            this.userInput = this.items[index]
+            this.index = index
+        },
+        updateTask: function(x) {
+            this.status= true
+            this.items.splice(this.index, 1, x)
+            this.userInput = ''
         }
     },
 })
